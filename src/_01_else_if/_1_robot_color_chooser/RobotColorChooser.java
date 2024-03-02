23137f488bbc0cd7c4694e83700a0b4d7688565e
locke <locke@192.168.1.66>
2024-03-01 17:02:38 -0800

    plz work at home locke, Work on the color opshon for ElseifRobotColorChooser

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -3,12 +3,35 @@ package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
 import java.awt.Color;
-
+import org.jointheleague.graphical.robot.Robot;
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		
+		int baseSize = 200;		//the size of the black part of the star
+		int flameSize = 130;
 		//1. Create a new Robot
-	    //2. Make the robot draw a shape (this will take more than one line of code)		
+		Robot rob = new Robot();
+		rob.penDown();
+		rob.setSpeed(100);
+		rob.setPenWidth(9);
+		
+		//2. Make the robot draw a shape (this will take more than one line of code)	rob.turn(45);
+		for(int ick = 0;ick<3000;ick++) { 
+			String  [] adventures = {"low lv Doge", "low lv Hit", "Low lv Health", "Low lv Damage", "Medium lv Doge", "Medium lv Hit", "Medium lv Health", "Medium lv Damage", "High lv Doge", "High lv Hit", "High lv Health", "High lv Damage"};
+			String  [] adventuresLvOne = {"low lv Doge", "low lv Hit", "Low lv Health", "Low lv Damage"};
+			String  [] leftOrRight = {"Left", "Right"};
+			String  [] roughOrSoft = {"Rough(lot of loot, DoT, and harder enemys)", "Smoth path(less loot, no DoT, easyer enemys)"};
+			int Adventures = JOptionPane.showOptionDialog(null, "Chose an adventure to go on", null, 0, 3, null, adventuresLvOne, null);
+rob.turn(45);
+		 rob.move(64);
+			
+
+			rob.turn(-40);
+			rob.move(flameSize);
+			rob.turn(170);
+			rob.move(flameSize);
+			rob.turn(64);
+			rob.move(baseSize);
+		}
 		//3. Set the pen width to 10
 		//4. Ask the user what color pen they would like the robot to draw with
 		//5. Use an if/else statement to set the pen color that the user requested
